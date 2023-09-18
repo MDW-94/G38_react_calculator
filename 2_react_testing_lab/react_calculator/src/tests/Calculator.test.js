f20fb1c5cf43106dfae6881c48e4970a433f7672
Matthew Wasylko <matthew.wasylko@gmail.com>
2023-09-18 14:49:35 +0100

    ran first two unit tests, both passed successfully.

--- 2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
+++ 2_react_testing_lab/react_calculator/src/tests/Calculator.test.js
@@ -14,5 +14,32 @@ describe('Calculator', () => {
     fireEvent.click(button4);
     expect(runningTotal.textContent).toEqual('4');
   })
+
+  it('should add 1 to 4 and get 5', () => {
+    const button1 = container.getByTestId('number1');
+    const addButton = container.getByTestId('operator-add');
+    const button3 = container.getByTestId('number4');
+    const equalButton = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(addButton);
+    fireEvent.click(button3);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual('5')
+  })
+
+  it('should subtract 4 from 7 and get 3', () => {
+    const button7 = container.getByTestId('number7');
+    const subtractButton = container.getByTestId('operator-subtract');
+    const button4 = container.getByTestId('number4');
+    const equalButton = container.getByTestId('operator-equals');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button7);
+    fireEvent.click(subtractButton);
+    fireEvent.click(button4);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual('3')
+
+  })
 })
 
